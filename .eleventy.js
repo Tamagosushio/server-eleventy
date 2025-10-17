40f941109d05059d3a132c1290aad6167e552469
tamagosushio <mitachi.ts75@gmail.com>
2025-10-18 00:19:56 +0900

    [add] コードブロックのハイライトとコピーを追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const codeClipboard = require("eleventy-plugin-code-clipboard");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("dateFormat", function (value) {
@@ -14,12 +17,21 @@ module.exports = function(eleventyConfig) {
       animated: true,
       limitInputPixels: false
     }
-  });  
+  });
+  // Add plugins
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(codeClipboard);
+  // Markdown Overrides
+  const markdownLibrary = markdownIt({
+    html: true,
+  }).use(codeClipboard.markdownItCopyButton); 
+  eleventyConfig.setLibrary("md", markdownLibrary);
   // Set directories to pass through to the _site folder
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/styles/");
   eleventyConfig.addPassthroughCopy("src/js/");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/apps/procon34-visualizer-web");
   // Watch scss folder for changes
   eleventyConfig.addWatchTarget("/src/assets/");
   eleventyConfig.addWatchTarget("/src/styles/");
@@ -31,7 +43,6 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addCollection("apps", function (collectionApi) {
 		return collectionApi.getFilteredByGlob("src/apps/*/index.html");
 	});
-
   eleventyConfig.addCollection("tagList", function(collection) {
     let tagSet = new Set();
     collection.getAll().forEach(function(item) {
@@ -46,8 +57,6 @@ module.exports = function(eleventyConfig) {
     });
     return [...tagSet].sort();
   });
-  // passthrough
-  eleventyConfig.addPassthroughCopy("src/apps/procon34-visualizer-web");
   return {
     dir: {
       input: "src",
